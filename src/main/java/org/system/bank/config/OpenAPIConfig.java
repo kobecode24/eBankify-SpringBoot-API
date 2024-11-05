d9b88beab3e1102449c76ca798968c2900cb34ee
kobe <kobecode24@outlook.com>
2024-11-05 13:45:40 +0100

    add OpenAPI configuration and CORS settings; remove application properties;

---
+++ src/main/java/org/system/bank/config/OpenAPIConfig.java
@@ -0,0 +1,31 @@
+package org.system.bank.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Banking System API")
+                        .description("API Documentation for Banking System")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080/api")
+                                .description("Development Server")
+                ));
+    }
+}
