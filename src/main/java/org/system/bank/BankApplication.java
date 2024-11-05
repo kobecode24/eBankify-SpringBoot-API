556b3786baa35603f256f3a47fe49684552f6143
kobe <kobecode24@outlook.com>
2024-11-05 13:43:47 +0100

    customized spring app banner

--- src/main/java/org/system/bank/BankApplication.java
+++ src/main/java/org/system/bank/BankApplication.java
@@ -7,7 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BankApplication.class, args);
+        SpringApplication app = new SpringApplication(BankApplication.class);
+        app.setBanner((environment, sourceClass, out) -> {
+            out.println(" ____  __.    ___.            _________            .___");
+            out.println("|    |/ _|____\\_ |__   ____   \\_   ___ \\  ____   __| _/____");
+            out.println("|      < /  _ \\| __ \\_/ __ \\  /    \\  \\/ /  _ \\ / __ |/ __ \\");
+            out.println("|    |  (  <_> ) \\_\\ \\  ___/  \\     \\___(  <_> ) /_/ \\  ___/");
+            out.println("|____|__ \\____/|___  /\\___  >  \\______  /\\____/\\____ |\\___  >");
+            out.println("        \\/         \\/     \\/          \\/            \\/    \\/");
+            out.println("                                                    ");
+
+        });
+        app.run(args);
     }
 
 }
